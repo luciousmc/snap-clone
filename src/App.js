bd86a677dcd22f9b1356d482a670f658685d37d6
Marlon Clay <luciousmc@icloud.com>
2021-06-10 15:57:00 -0700

    preview component added configuring pushing data to firestore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
+import Preview from './Preview';
 
 function App() {
   return (
     <div className="app">
-      <h1>MarChat!</h1>
-      <WebcamCapture />
+      <Router>
+        <div className="app__body">
+          <Switch>
+            <Route path="/preview">
+              <Preview />
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -3,6 +3,8 @@ import Webcam from "react-webcam";
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import { useDispatch } from "react-redux";
 import { setCameraImage } from "./features/cameraSlice";
+import { useHistory } from "react-router-dom";
+import './WebcamCapture.css';
 
 const videoConstraints = {
   width: 250,
@@ -13,15 +15,17 @@ const videoConstraints = {
 function WebcamCapture() {
   const webcamRef = useRef(null);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
 
     dispatch(setCameraImage(imageSrc))
+    history.push('/preview')
   }, [webcamRef])
   
   return (
-    <div className="webcamcapture">
+    <div className="webcamCapture">
       <Webcam 
         audio={false}
         height={videoConstraints.height}
